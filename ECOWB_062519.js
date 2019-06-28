4d4e35f0d462ed1e8f745f866bab879c7c9e44b8
Joshsumers <sumersspatialdevelopment@gmail.com>
2019-06-27 23:10:24 -0500

    archived old Script ; Setup Visual Parameters

--- ECOWB_062519.js
+++ ECOWB_062519.js
@@ -1,19 +1,14 @@
 //// ECO-WB Water Hycanith Project ////
 //// Draft Script 1 ////
 //// By: Joshua Sumers ////
-//// Date: 06-25-2019 ////
+//// Start Date: 06-25-2019 ////
+//// Update Date: 06-26-2019 ////
 
 //Set two study areas to eliminate overlap
-var NorthGulf = /* color: #ffc82d */ee.Geometry.Polygon(
-        [[[34.45449542486756, -0.1629348170856311],
-          [34.64126300299256, -0.44033548712972165],
-          [34.94888019049256, -0.2590645250529491],
-          [34.69344806158631, -0.028352516004205346]]]),
-    SouthGulf = /* color: #ff0000 */ee.Geometry.Polygon(
-        [[[34.64072130588943, -0.43918920351144],
-          [34.45410260311701, -0.16191296042985115],
-          [33.9875801906428, -0.23635574796800354],
-          [34.24987877462718, -0.6459611028902127]]]);
+var North = ee.FeatureCollection("users/joshsumers1996/North_Gulf"),
+    South = ee.FeatureCollection("users/joshsumers1996/South_Gulf");
+
+
 
 // import sentinel imagery
 var Sent1 = ee.ImageCollection('COPERNICUS/S1_GRD');
@@ -26,7 +21,6 @@ var vvvh = Sent1
  .filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VH'))
 // set to IW mode
  .filter(ee.Filter.eq('instrumentMode', 'IW'));
-
 //filter to north area
 var vvvhN = vvvh
  .filterBounds(NorthGulf)
@@ -35,6 +29,7 @@ var vvvhN = vvvh
 var vvvhS = vvvh
  .filterBounds(SouthGulf)
   .map(function(image){return image.clip(SouthGulf)});
+var
 
 //set visualization parameters
 var visParm = {min: [-25, -20, -5], max: [0, 10, 25]}
--- ECOWB_NE_Gulf.js
+++ ECOWB_NE_Gulf.js
@@ -2,17 +2,20 @@
 //// NorthEast Gulf Draft Script ////
 //// By: Joshua Sumers ////
 //// Start Date: 06-25-2019 ////
-//// Update Date: 06-26-2019 ////
+//// Update Date: 06-27-2019 ////
 
 //Set NE study Area
 var NorthGulf = ee.FeatureCollection("users/joshsumers1996/North_Gulf");
 
 //set start Date
-var Start = '2018-01-01';
+var Start = '2017-05-06';
 
 //set End Date
 
-var End = '2018-12-31';
+var End = '2017-05-08';
+
+//imagery date for test
+//var TDate = '2017-05-07';
 
 // import sentinel imagery
 var Sent1 = ee.ImageCollection('COPERNICUS/S1_GRD');
@@ -26,27 +29,29 @@ var vvvh = Sent1
 // set to IW mode
  .filter(ee.Filter.eq('instrumentMode', 'IW'))
  //Date
- .filter(ee.Filter.date(Start,End))
+ .filter(ee.Filter.date(Start, End))
  //filter North
  .filterBounds(NorthGulf)
  //Clip North
  .map(function(image){return image.clip(NorthGulf)});
 
-//filtering by view angles
-var vhAscending = vvvh.filter(ee.Filter.eq('orbitProperties_pass', 'ASCENDING'));
-var vhDescending = vvvh.filter(ee.Filter.eq('orbitProperties_pass', 'DESCENDING'));
-
-//creating composite
-var NComp = ee.Image.cat([
-  vhAscending.select('VH').mean(),
-  ee.ImageCollection(vhAscending.select('VV').merge(vhDescending.select('VV'))).mean(),
-  vhDescending.select('VH').mean()
-]).focal_median();
-
-
 
 //set visualization parameters
-var visParm = {min: - 25, max: 5};
+var visParm = {Bands: 'VV,VH', min: -30, max: 5};
 
- Map.addLayer(NComp, visParm, 'North');
- Map.centerObject(NorthGulf, 10);
+ Map.addLayer(vvvh, visParm, 'North');
+ Map.centerObject(NorthGulf, 11);
+
+
+//Do you want to export?
+var VExport = false;
+
+//export video
+if (VExport === true) {
+Export.video.toDrive({
+    collection: Video,
+    description: "VideoOfWaterHy" ,
+    scale: 30,
+    framesPerSecond: 10,
+    region: NorthGulf
+})}
