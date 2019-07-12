8e2c3609196ab067b3a5c758bdc356c2b656443a
Joshsumers <sumersspatialdevelopment@gmail.com>
2019-07-11 23:22:47 -0500

    Export image added, start of code to get vv/vh values on click

--- ECOWB_NE_Gulf.js
+++ ECOWB_NE_Gulf.js
@@ -2,7 +2,7 @@
 //// NorthEast Gulf Draft Script ////
 //// By: Joshua Sumers ////
 //// Start Date: 06-25-2019 ////
-//// Update Date: 06-27-2019 ////
+//// Update Date: 07-11-2019 ////
 
 //Set NE study Area
 var NorthGulf = ee.FeatureCollection("users/joshsumers1996/North_Gulf");
@@ -18,7 +18,7 @@ var End = '2017-05-08';
 //var TDate = '2017-05-07';
 
 // import sentinel imagery
-var Sent1 = ee.ImageCollection('COPERNICUS/S1_GRD');
+
 
 //filter sentinel imagery to include vv+vh
 var vvvh = Sent1
@@ -35,15 +35,44 @@ var vvvh = Sent1
  //Clip North
  .map(function(image){return image.clip(NorthGulf)});
 
-
+var SImage = vvvh.median().select('VV','VH');
 //set visualization parameters
 var visParm = {Bands: 'VV,VH', min: -30, max: 5};
 
  Map.addLayer(vvvh, visParm, 'North');
+ Map.addLayer(SImage, visParm, 'Median image');
  Map.centerObject(NorthGulf, 11);
+ Map.style().set('cursor', 'crosshair');
 
 
-//Do you want to export?
+ var inspector = ui.Panel([ui.Label('Click to get VV / VH Values')]);
+ Map.add(inspector);
+
+var Click = Map.onClick(function(coords) {
+  var Inspectionpoint = ee.Geometry.Point(coords.lon, coords.lat);
+  //var PointValue = Inspectionpoint.select('VV');
+  var sample = SImage.sample(Inspectionpoint,10);
+  inspector.widgets().set(0, ui.Label({
+    value: 'VV/VH' + sample,
+    style: {color: 'gray'}
+  }))});
+
+//Do you want to export an image?
+var IExport = false;
+
+//export image
+if (IExport === true){
+  Export.image.toDrive({
+    image: SImage,
+    description: "WaterHycanith",
+    maxPixels: 1e10,
+    crs: "EPSG:3857",
+    scale: 10,
+    fileFormat: 'GeoTIFF',
+  })
+}
+
+//Do you want to export video?
 var VExport = false;
 
 //export video
